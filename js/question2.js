e9006fa8a986af43c09eb7688124362b16766bf9
Maren Lilleberre <marenlilleberre@gmail.com>
2021-10-01 16:23:27 +0200

    assignment complete

--- js/question2.js
+++ js/question2.js
@@ -2,24 +2,28 @@
 
 const apiUrl = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=ff24fd47483e4941944876fe192adba4"
 const apiContainer = document.querySelector('.API-container');
+const loader = document.querySelector('.lds-roller');
 
 async function getGames(){
-    
-    const response = await fetch(apiUrl);
-    const jsonResults = await response.json();
-    const games = jsonResults.results;
 
-    console.log(games);
+    try{
+        const response = await fetch(apiUrl);
+        const jsonResults = await response.json();
+        const games = jsonResults.results;
 
-    for (let i = 0; i < games.length; i++) {
-        const game = games[i];
-        apiContainer.innerHTML += `<h2>${game.name}</h2>  <p> Rating : ${game.rating} Tags: ${game.tags.length}</p>`
-        if (i === 7){
-            break;
-        }
-        
+        for (let i = 0; i < games.length; i++) {
+            const game = games[i];
+            loader.style.display='none';
+            apiContainer.innerHTML += `<div class=games><h2>${game.name}</h2>   <p> <span>Rating: </span>${game.rating} <span>Tags:</span> ${game.tags.length}</p></div>`
+            if (i === 7){
+                break;
+            }
+        } 
+    } catch (error){
+        loader.style.display='none';
+        apiContainer.innerHTML = `We're sorry, the error "${error.name}"" occurred. <br>
+        <button>Report error</button>`;
     }
-
 }
 
 getGames();
